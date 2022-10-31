23c681f3f3582a1064e3a3f813f4e45210fe5a93
Rioni <nrosa01@ucm.es>
2022-10-31 15:47:18 +0100

    Correciones parte visual board

--- GameLogic/src/main/java/com/example/gamelogic/entities/Board.java
+++ GameLogic/src/main/java/com/example/gamelogic/entities/Board.java
@@ -6,6 +6,9 @@ import com.example.gamelogic.states.MainGameLogic;
 public class Board extends Entity{
     int[][] board;
     int rows, cols, gapSize;
+    int paddingHorizontal, paddingVertical = 0;
+
+    int cellWidth, cellHeight, widthArea, heightArea;
 
     public Board(IEngine engine, int rows, int cols, int width, int gapSize) {
         super(engine);
@@ -15,6 +18,21 @@ public class Board extends Entity{
         this.width = width;
         this.height = (int) (width * rows / cols);
         this.gapSize = gapSize;
+        init();
+    }
+
+    private void init()
+    {
+        widthArea = width - (gapSize + paddingHorizontal);
+        heightArea = height - (gapSize + paddingVertical);
+
+        cellWidth = (widthArea - (cols-1)*(gapSize))/cols;
+        cellHeight = (heightArea - (rows-1)*(gapSize))/rows;
+
+        // Sometimes, the cellWidth and cellHeight with the gap wont cover the whole width and height, so we add padding
+        // to space the cells evenly
+        paddingHorizontal = (widthArea - (cols-1)*(gapSize) - cols*cellWidth)/2;
+        paddingVertical = (heightArea - (rows-1)*(gapSize) - rows*cellHeight)/2;
     }
 
     @Override
@@ -28,22 +46,21 @@ public class Board extends Entity{
         graphics.fillRectangle(posX, posY,width, height);
         graphics.setColor(123,123,123);
 
-        // Instead of scaling the canvas, we simulate that the drawing area for the cells is smaller
-        // by substracting gap. This way is easier to do.
-        int widthArea = width - (gapSize);
-        int heightArea = height - ( gapSize);
-
-        int cellWidth = (widthArea - (cols-1)*(gapSize))/cols;
-        int cellHeight = (heightArea - (rows-1)*(gapSize))/rows;
-
-        // There are some cases where the space doesnt look right, but I dont know why
         for (int row = 0; row < rows; row++) {
             for (int col = 0; col < cols; col++) {
-                int cellPosX = posX - widthArea/2 + col*(cellWidth+gapSize) + cellWidth/2;
-                int cellPosY = posY - heightArea/2 + row*(cellHeight+gapSize) + cellHeight/2;
-                graphics.fillRectangle(cellPosX, cellPosY, cellWidth, cellHeight);
+                graphics.fillRectangle( getCellPosX(col),  getCellPosY(row), cellWidth, cellHeight);
             }
         }
+    }
+
+    int getCellPosX(int col)
+    {
+        return posX - widthArea/2 + col*(cellWidth+gapSize) + cellWidth/2 + paddingHorizontal;
+    }
+
+    int getCellPosY(int row)
+    {
+        return posY - heightArea/2 + row*(cellHeight+gapSize) + cellHeight/2 + paddingVertical;
     }
 
     @Override
