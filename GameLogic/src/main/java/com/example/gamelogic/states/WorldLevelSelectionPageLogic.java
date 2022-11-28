5bed3893f27a7c3a863667988ef369013b5f1206
Rioni <nrosa01@ucm.es>
2022-11-28 23:07:13 +0100

    Mas avances

--- GameLogic/src/main/java/com/example/gamelogic/states/WorldLevelSelectionPageLogic.java
+++ GameLogic/src/main/java/com/example/gamelogic/states/WorldLevelSelectionPageLogic.java
@@ -1,23 +1,53 @@
 package com.example.gamelogic.states;
 
 import com.example.engine.IEngine;
+import com.example.engine.IFont;
 import com.example.engine.IImage;
 import com.example.gamelogic.entities.Button;
 import com.example.gamelogic.entities.IInteractableCallback;
 import com.example.gamelogic.levels.WorldLevelType;
 
-public class WorldLevelSelectionPageLogic extends AbstractState{
+public class WorldLevelSelectionPageLogic extends AbstractState {
     Button returnButton;
     IImage arrow;
+    Button levels[][];
+    IFont tittleFont; // TODO: Crear una entidad que sea simplemente un texto
+    String text;
 
     protected WorldLevelSelectionPageLogic(IEngine engine, WorldLevelType type) {
         super(engine);
+        text = type.toString();
     }
 
     @Override
     public boolean init() {
-        try
-        {
+        try {
+            tittleFont = graphics.newFont(engine.getAssetsPath() + "fonts/Roboto-Regular.ttf", 24, true);
+
+            int xPos = 0;
+            int yPos = 100;
+
+            int width = (LOGIC_WIDTH - (5 * 10)) / 5;
+            int height = width;
+            int spacing = (LOGIC_WIDTH - (5 * width)) / 6;
+
+            levels = new Button[4][5];
+            for (int i = 0; i < 4; i++) {
+                for (int j = 0; j < 5; j++) {
+                    // Calculate the x and y position of the button
+                    xPos = (j * width) + ((j + 1) * spacing) + width / 2;
+                    yPos = (i * height) + ((i + 1) * spacing) + (LOGIC_HEIGHT / 2);
+
+                    // Create the button
+                    levels[i][j] = new Button(xPos, yPos, width, height, engine);
+                    levels[i][j].setBorderSize(4);
+                    levels[i][j].setBorderColor(183, 210, 79);
+                    levels[i][j].setBackgroundColor(240, 240, 240, 255);
+                    levels[i][j].setPadding(5, 5);
+                    addEntity(levels[i][j]);
+                }
+            }
+
             arrow = graphics.newImage(engine.getAssetsPath() + "images/arrow.png");
             returnButton = new Button(25, 25, 30, 30, engine);
             returnButton.setImage(arrow);
@@ -40,7 +70,13 @@ public class WorldLevelSelectionPageLogic extends AbstractState{
             e.printStackTrace();
             return false;
         }
-
         return true;
+    }
+
+
+    @Override
+    public void render() {
+        super.render();
+        graphics.drawTextCentered(text, LOGIC_WIDTH/2, (LOGIC_HEIGHT / 2) - 100, tittleFont);
     }
 }
