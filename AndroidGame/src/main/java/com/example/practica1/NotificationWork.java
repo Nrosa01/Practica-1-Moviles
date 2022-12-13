353fe3c1e3e032adf572ba826f2ae4e21a84dd26
Javier Meitín Moreno <jmeitin@ucm.es>
2022-12-13 21:38:42 +0100

    sigue sin saber hacer log

--- AndroidGame/src/main/java/com/example/practica1/NotificationWork.java
+++ AndroidGame/src/main/java/com/example/practica1/NotificationWork.java
@@ -4,11 +4,17 @@ import android.content.Context;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 
 public class NotificationWork extends Worker {
     private static final String TAG = "NotificationWork";
+    public static final String NOTI_MGR = "NOTI_NGR";
+    public static final String BUILDER = "BUILDER";
+    //public static NotificationManagerCompat notificationManager;
+    //public static NotificationCompat.Builder builder;
 
     public NotificationWork(
             @NonNull Context context,
@@ -19,8 +25,10 @@ public class NotificationWork extends Worker {
     @NonNull
     @Override
     public Result doWork() {
+        //NotificationManagerCompat notificationManager = getInputData().getInt(NOTI_MGR, 0);
+        //notificationManager.notify(1, builder.build()); //notificationId
         Log.i("Cosa ","aaaaaaaaaaaaaaaa");
-        Log.e(TAG, "doWork: Work is done.");
+        //Log.e(TAG, "doWork: Work is done.");
         return Result.success();
     }
 }
