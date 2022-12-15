62b808ef274ad63c19c1a8942beeaefdda7ade5b
Javier Meitín Moreno <jmeitin@ucm.es>
2022-12-15 17:59:54 +0100

    Las notificaciones ya se llamasn en doWork

--- AndroidGame/src/main/java/com/example/practica1/MainActivity.java
+++ AndroidGame/src/main/java/com/example/practica1/MainActivity.java
@@ -51,8 +51,6 @@ public class MainActivity extends AppCompatActivity  {
     private AdView mAdView;
     private InterstitialAd mInterstitialAd;
 
-    NotificationCompat.Builder builder;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -85,53 +83,20 @@ public class MainActivity extends AppCompatActivity  {
         AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
 
-        //----------------------------------------------------------------------
-        createNotificationChannel();
 
-        // CREAR INTENT: Create an explicit intent for an Activity in your app
-        Intent intent = new Intent(this, MainActivity.class); //triple
-        intent.putExtra("someKey", 115);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE);
 
-        //CREAR NOTIFICACION:
-        //var CHANNEL_ID = "My Notification";
-        builder = new NotificationCompat.Builder(this,  "My Notification") //CHANNEL_ID
-                .setSmallIcon(R.drawable.bell) //R.drawable.notification_icon
-                .setContentTitle("Notificación del Nonograma")
-                .setContentText("Eres un grande!")
-                // Si quieres que ocupe mas de 1 linea la notificacion
-                //.setStyle(NotificationCompat.BigTextStyle()
-                  //      .bigText("Much longer text that cannot fit one line..."))
-                //-----------------
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                // Set the intent that will fire when the user taps the notification
-                .setContentIntent(pendingIntent)
-                .setAutoCancel(true);
-        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
-        //MOSTRAR NOTIFICACION--------------------------------------------------
-        // notificationId is a unique int for each notification that you must define
-        notificationManager.notify(1, builder.build()); //notificationId
 
-        WorkManager workManager = WorkManager.getInstance(this); //application.applicationContext!!
-        //
-        //String ENDPOINT_REQUEST = "ENDPOINT_REQUEST, endPoint";
-        //Data data = new Data.Builder()
-         //       .putString(ENDPOINT_REQUEST, "ENDPOINT_REQUEST, endPoint");// ENDPOINT_REQUEST, endPoint
-        //Crear work
-        PeriodicWorkRequest work = new
-                PeriodicWorkRequest.Builder(NotificationWork.class, 10, TimeUnit.SECONDS)
-                .setConstraints(new Constraints.Builder()
-                        .setRequiresCharging(true)
-                        .build()
-                )
-                //.setInputData(data.build())
+        //Log.i("Cosa ","BBBBBBBBBB");
+        Constraints constraints = new Constraints.Builder().setRequiredNetworkType(NetworkType.CONNECTED).build();
+        PeriodicWorkRequest build = new PeriodicWorkRequest.Builder(NotificationWork.class, 300, TimeUnit.SECONDS)
+                .addTag("TAG")
+                .setConstraints(constraints)
                 .build();
-        //enque work en workmanager
-        WorkManager.getInstance(this).enqueueUniquePeriodicWork(
-                "sendLogs",
-                ExistingPeriodicWorkPolicy.KEEP,
-                work);
+
+        WorkManager instance = WorkManager.getInstance();
+        if (instance != null) {
+            instance.enqueueUniquePeriodicWork("TAG", ExistingPeriodicWorkPolicy.REPLACE, build);
+        }
 
 
         //----------------------------------------------------------------------------------
@@ -159,22 +124,7 @@ public class MainActivity extends AppCompatActivity  {
         AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
     }
-    //CREAR CANAL ==> si quieres meter sonidos y mierdas:
-    private void createNotificationChannel() {
-        // Create the NotificationChannel, but only on API 26+ because
-        // the NotificationChannel class is new and not in the support library
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            CharSequence name = "My Notification";//getString(R.string.channel_name);
-            String description = "My Notification Description";//getString(R.string.channel_description);
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = new NotificationChannel("My Notification", name, importance); //CHANNEL_ID
-            channel.setDescription(description);
-            // Register the channel with the system; you can't change the importance
-            // or other notification behaviors after this
-            NotificationManager notificationManager = getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(channel);
-        }
-    }
+
 
     @Override
     protected void onResume() {
