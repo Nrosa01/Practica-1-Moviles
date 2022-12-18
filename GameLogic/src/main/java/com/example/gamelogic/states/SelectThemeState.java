e114c86002c9f999eb89a1f1a6cbefeb79e846c2
Javier Meitín Moreno <jmeitin@ucm.es>
2022-12-18 11:11:10 +0100

    Colores bloqueados (falta que se desbloquee al completar un nivel)

--- GameLogic/src/main/java/com/example/gamelogic/states/SelectThemeState.java
+++ GameLogic/src/main/java/com/example/gamelogic/states/SelectThemeState.java
@@ -16,15 +16,15 @@ public class SelectThemeState extends AbstractState{
     Button returnButton;
 
     protected int NUM_THEMES = 3;
-    protected int unlockedThemes = 0; //123, 123, 123
+    private boolean[] unlockedThemes = {true, false, false}; //el primero esta desbloqueado por defecto
     protected Color[][] themes = {
             {new Color(255, 255, 255),new Color(123, 123, 123), new Color(123, 123, 255), new Color(255, 123, 123)},
             {new Color(0, 255, 0),new Color(255, 255, 0), new Color(123, 123, 255), new Color(255, 123, 123)},
             {new Color(255, 0, 0),new Color(0, 255, 255), new Color(123, 123, 255), new Color(255, 123, 123)}};
 
-    public void DesbloquearColor(){
-        if(unlockedThemes < NUM_THEMES - 1)
-            unlockedThemes ++;
+    public void DesbloquearColor(int i){
+        if(i < NUM_THEMES) // 2 < 3
+            unlockedThemes[i] = true;
     }
 
     public SelectThemeState(IEngine engine) {
@@ -64,26 +64,37 @@ public class SelectThemeState extends AbstractState{
             //BOTONES DE SELECCION
             int buttonSize = graphics.isLandscape() ? LOGIC_WIDTH / 5 : LOGIC_WIDTH / 8;
             int gapSize = buttonSize / 2;
+            IImage unlockedImg = graphics.newImage(engine.getAssetsPath() + "images/unlock.png");
+            IImage lockedImg = graphics.newImage(engine.getAssetsPath() + "images/lock.png");
 
             for (int col = 0; col < NUM_THEMES; col++) {
                 int buttonY = graphics.isLandscape() ? 320 : 195;
                 int buttonX = graphics.isLandscape() ? (gapSize + buttonSize) * (col + 1) - gapSize : 110 + (gapSize + buttonSize) * (col + 1) - gapSize;
                 Button button = new Button(buttonX, buttonY, buttonSize, buttonSize, engine);
-                //button.setText(texts[row][col], fontBold);
-                //final int finalRow = row;
-                final int finalCol = col;
-                button.setCallback(new IInteractableCallback() {
-                    @Override
-                    public void onInteractionOccur() {
-                        try {
-                            //engine.setState(new MainGameLogic(engine, texts[finalRow][finalCol]));
-                            setColors(themes[finalCol][0], themes[finalCol][1], themes[finalCol][2], themes[finalCol][3]);
-                            graphics.setClearColor(themes[finalCol][0].r, themes[finalCol][0].g, themes[finalCol][0].b);
-                        } catch (Exception e) {
-                            e.printStackTrace();
+                button.setBackgroundColor(themes[col][0].r, themes[col][0].g, themes[col][0].b);
+                if (unlockedThemes[col] == true) {
+                    button.setImage(unlockedImg);
+                    //button.setText(texts[row][col], fontBold);
+                    //final int finalRow = row;
+                    final int finalCol = col;
+                    button.setCallback(new IInteractableCallback() {
+                        @Override
+                        public void onInteractionOccur() {
+                            try {
+                                //engine.setState(new MainGameLogic(engine, texts[finalRow][finalCol]));
+                                setColors(themes[finalCol][0], themes[finalCol][1], themes[finalCol][2], themes[finalCol][3]);
+                                graphics.setClearColor(themes[finalCol][0].r, themes[finalCol][0].g, themes[finalCol][0].b);
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
                         }
-                    }
-                });
+                    });
+                }
+
+                else{
+                    button.setImage(lockedImg);
+                }
+
                 addEntity(button);
             }
 
